6e7160c69752b4204fbc80525fb13de0712266a3
jjmau14 <jjmau14@gmail.com>
2017-04-22 08:11:10 -0600

    added new lambda and copy ctor test

--- templates_lambda.cc
+++ templates_lambda.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 #include <algorithm>
 #include <set>
 
@@ -14,6 +13,7 @@ class Point{
 		Point(T x_, T y_) : x(x_), y(y_){
 			cout << "Initialized P\n";
 		}
+
 		T getx() const {return x;}
 		T gety() const {return y;}
 		friend ostream &operator<<(ostream &out, const Point<T> &p){
@@ -33,16 +33,33 @@ int main(){
 	// Templates
 	Point<double> a(3.4, 2.1);
 	cout << a;
-	
+	Point<double> b(a);
+        cout << b;
+
 	// Functors
 	Adder add;
 	cout << "Adding resolves to: " << add(12.3, 1039.32) << endl;
 	
 	// Lambda Functions
-	set<int> s = {2,3,4,5,6,23,5,6,34,23,34,5,65,32,213,23,45,63,2};
-	// iterates until it finds a value > 20 begining at the set begin() and
-	// ending at set end() using a lambda function to determine > 20
+	vector<int> s = {2,3,4,5,6,23,5,6,34,23,34,5,65,32,213,23,45,63,2};
+	// iterates until it finds a value > 20 begining at the v begin() and
+	// ending at v end() using a lambda function to determine > 20
+        
+        // find_if
 	auto itor = find_if(s.begin(), s.end(), [](int n){return n>20;});
 	// Should print 23
 	cout << *itor << endl;
+        
+        //copy_if
+        itor = copy_if(s.begin(), s.end(), s.begin(), [](int n){
+            return n > 20 && n <= 63;
+        });
+        
+        // itor now contains the number of elements coppied so itor - begin() 
+        // for resize
+        s.resize(itor-s.begin());
+        for (auto x : s){
+            cout << x << ' ';
+        }
+        cout << endl;
 }
